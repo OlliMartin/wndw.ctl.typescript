bd803a57962c29eb080eff453ab92da81d899af8
OlliMartin <scholl3ss93@gmail.com>
2025-01-05 21:32:13 +0100

    Add generated backend

--- apps/iobroker.acaad-admin/src/App.tsx
+++ apps/iobroker.acaad-admin/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { GenericApp, GenericAppProps, I18n } from '@iobroker/adapter-react-v5';
 
@@ -75,7 +74,6 @@ class App extends GenericApp {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
