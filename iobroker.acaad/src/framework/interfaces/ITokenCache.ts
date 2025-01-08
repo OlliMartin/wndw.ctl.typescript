f426f81e8e41d9c052dd4015a27ed415ec85e73e
OlliMartin <scholl3ss93@gmail.com>
2025-01-08 09:35:42 +0100

    Cleanup build directory

--- iobroker.acaad/src/framework/interfaces/ITokenCache.ts
+++ iobroker.acaad/src/framework/interfaces/ITokenCache.ts
@@ -1,6 +1,7 @@
 import { OAuth2Token } from "../model/auth/OAuth2Token";
 import { AcaadAuthentication } from "../model/auth/AcaadAuthentication";
+import Option from "../fp/Option";
 
 export interface ITokenCache {
-    getAsync(authentication: AcaadAuthentication): Promise<OAuth2Token | undefined>;
+    getAsync(authentication: AcaadAuthentication): Promise<Option<OAuth2Token>>;
 }
